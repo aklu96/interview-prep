2996c0ddf03a718cd9a18423f71ef784d7cd28bc
Alex Klyuev <aklyuev96@gmail.com>
2021-08-02 18:41:05 -0700

    heap sort

---
+++ algorithms/sorting/heapSort.js
@@ -0,0 +1,28 @@
+const Heap = require('../../../javascript-data-structures/binaryHeap.js');
+
+
+
+const heapSort = (comparator, nums) => {
+  const result = [];
+  const heap = new Heap(comparator);
+  for (let i = 0; i < nums.length; i++) {
+    heap.insert(nums[i]);
+  }
+  // by inserting every number into the heap, we maintain order
+  // over which element is the smallest. by removing the root
+  // one by one, we return a sorted array in ascending order
+  for (let i = 0; i < nums.length; i++) {
+    result.push(heap.removeRoot());
+  }
+  return result;
+}
+
+// use a min heap to sort an array from smallest to greatest
+console.log(heapSort((a, b) => a < b, [4, 5, 1, 7, 8]));
+
+// use a max heap to sort an array from greatest to smallest
+console.log(heapSort((a, b) => a > b, [4, 5, 1, 7, 8]));
+
+// you can use a min heap sort implementation of a given size to get
+// the biggest k elements in an array, or vice versa. just remember that
+// it's inverted counter-intuitively here
