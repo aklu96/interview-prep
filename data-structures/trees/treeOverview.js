c4bb9b7a37ec404e79c17935cea51b22bfdf114e
Alex Klyuev <aklyuev96@gmail.com>
2021-08-04 12:39:24 -0700

    trees

---
+++ data-structures/trees/treeOverview.js
@@ -0,0 +1,27 @@
+/*
+
+Binary tree:
+
+Node {
+  this.value = value
+  this.left = Node
+  this.right = Node
+}
+
+N-ary tree:
+
+Node {
+  this.value = value
+  this.children = [Nodes]
+}
+
+Trie:
+
+Node {
+  this.value = character
+  this.children = [Nodes]
+}
+
+Recall that with trees, time complexities depend on if they are balanced or not.
+
+*/
